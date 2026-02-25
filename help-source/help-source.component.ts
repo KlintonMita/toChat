bac4621a61f667f3c193f4522dc93fa057012c10
Klinton Mita <mitatoni22@gmail.com>
2026-02-25 22:22:07 +0100

    first commit

---
+++ help-source/help-source.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-source',
+  standalone: true,
+  imports: [],
+  templateUrl: './help-source.component.html',
+  styleUrl: './help-source.component.scss'
+})
+export class HelpSourceComponent {
+
+}
