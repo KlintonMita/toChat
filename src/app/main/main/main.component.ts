336dcab3fb6d2605ed59da02af7bc7c24751500f
Klinton Mita <mitatoni22@gmail.com>
2026-04-13 12:57:16 +0200

    Discover page finsished and final desgin

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatMenuModule, MatIconModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
